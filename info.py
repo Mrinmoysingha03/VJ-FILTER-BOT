a33563fbc4cacd60cf661843aae3de626433bb79
Mrinmoysingha03 <mrinmoysinghasarkar03@gmail.com>
2025-06-21 02:21:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,7 +106,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkcents.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'cd98fa9fb48f010261833c83ba3613559e97f593')
 
