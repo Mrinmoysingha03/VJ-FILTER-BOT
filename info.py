10fb5cb34997c54452158d8d17a1fd73f7a4c310
Mrinmoysingha03 <mrinmoysinghasarkar03@gmail.com>
2025-08-21 01:41:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,8 +107,8 @@ VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'vplink.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '1b49ab613116c74d59e4786cc5069049cf7c0c95')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arolinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'cf086f45711db6507b7150a017d71e4dc640c157')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
@@ -166,6 +166,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
